16e29204990073c0842b19bdef6fca81281f278c
Ann <anna.kurennaya38@gmail.com>
2020-03-11 12:07:51 +0200

    first version

---
+++ Lab3/Array.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Array
+    {
+        private int[,] arr;
+        private int rows, columns;
+        bool error;
+
+        public Array(int r, int c)
+        {
+            arr = new int[r, c];
+            rows = r;
+            columns = c;
+            error = false;
+        }
+        public void GenData()
+        {
+            Random rn = new Random();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    arr[i, j] = rn.Next(0, 25);
+                }
+            }
+        }
+        public int this[int index]
+        {
+            get
+            {
+                --index;
+                if (ok(index))
+                {
+                    error = false;
+                    int sum = 0;
+                    for (int i = 0; i < rows; i++)
+                        sum += arr[i, index];
+                    return sum;
+                }
+                else
+                {
+                    error = true;
+                    return 0;
+                }
+            }
+        }
+        private bool ok(int index)
+        {
+            if (index >= 0 & index < columns) return true;
+            return false;
+        }
+         public double SumSquare {
+            get
+            {
+                double sumsq = 0;
+                for(int i = 0; i< rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        sumsq += Math.Pow(arr[i, j],2);
+                    }
+                }
+                                              
+                sumsq /= rows * columns;
+                sumsq = Math.Sqrt(sumsq);
+                return sumsq;
+            }
+         }
+    }
+}
---
+++ Lab3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Клас інкапсулює двовимірний масив з елементів цілого типу.
+//Створити одновимірний індексатор, що повертає суму всіх елементів
+//заданого індексом стовпця масиву.Створити властивість, доступну
+//для читання закритого елементу-даного, що містить
+//середньоквадратичне значення всіх елементів масиву
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array array = new Array(3 , 4);
+            array.GenData();
+            int sum;
+            sum = array[2];
+            double sqrt;
+            sqrt = array.SumSquare;
+        }
+    }
+}
